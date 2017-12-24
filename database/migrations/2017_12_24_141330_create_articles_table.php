3ef1ace89b7bdfde49c6ae2bb5b2cdf6d3b56e49
Strift <lau.cazanove@gmail.com>
2017-12-24 20:21:55 +0400

    Initial commit

---
+++ database/migrations/2017_12_24_141330_create_articles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('writer');
+            $table->string('topic');
+            $table->string('url')->nullable();
+            $table->string('header_url')->nullable();
+            $table->dateTime('approved_at')->nullable();
+            $table->dateTime('corrected_at')->nullable();
+            $table->date('date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
